c3b1ffa4496e64296963f7b68e1118285ef5635f
susmitha-sunny <susmithasunny317@gmail.com>
2021-12-03 13:12:12 +0530

    final draft

--- src/main/java/com/gotravel/zuulapigatewayservice/ZuulApiGatewayServiceApplication.java
+++ src/main/java/com/gotravel/zuulapigatewayservice/ZuulApiGatewayServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
